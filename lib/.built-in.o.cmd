9c443d800af1584915a746a2d9c58171deea130b
Mugdha Gawde <mugdha.gawde@gmail.com>
2017-05-15 10:09:09 -0500

    cleared

--- lib/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_lib/built-in.o :=  ld -z max-page-size=0x1000 -melf_x86_64 -dp  -r -o lib/built-in.o lib/bitmap.o
--- src/fs/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_src/fs/built-in.o :=  rm -f src/fs/built-in.o; ar rcs src/fs/built-in.o
